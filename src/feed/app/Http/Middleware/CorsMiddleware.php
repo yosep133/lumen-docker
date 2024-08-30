08b0094fd37a1c27cac73df952fa7528f8c691c8
YosepBelfridus <yosep06@gmail.com>
2024-08-30 09:50:41 +0700

    update cors feed

--- src/feed/app/Http/Middleware/CorsMiddleware.php
+++ src/feed/app/Http/Middleware/CorsMiddleware.php
@@ -8,7 +8,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => 'http://localhost:8000',
+            'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Headers'     => 'Authorization, x-csrf-token'
